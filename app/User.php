9715b5231fecc8c4ed528c4b1f3d3e9ffcbe9d84
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-12-04 20:41:33 +0900

    `bin/check-coding-rule --fix` で自動修正

--- app/User.php
+++ app/User.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable {
+class User extends Authenticatable
+{
     use Notifiable;
 
     /**
@@ -13,7 +15,7 @@ class User extends Authenticatable {
      * @var array
      */
     protected $fillable = [
-        "name", 'email', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
--- public/js/hoge.js
+++ public/js/hoge.js
@@ -1,12 +1,11 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', function (event) {
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(event) {
     console.log('DOM fully loaded and parsed');
 
     var hoge = 'Hoge';
-  var el = document.querySelector('.title');
-    el.addEventListener("click", function(){
-        alert('Laravel!!!')
-    }, false );
-
-
+    var el = document.querySelector('.title');
+    el.addEventListener('click', function() {
+        alert('Laravel!!!');
+    }, false);
 });

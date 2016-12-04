873e031b9bf2861ac813cbd044407421b87c1d18
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-12-04 20:44:00 +0900

    未使用変数などの自動修正できない違反を修正

--- public/js/hoge.js
+++ public/js/hoge.js
@@ -1,9 +1,6 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    console.log('DOM fully loaded and parsed');
-
-    var hoge = 'Hoge';
+document.addEventListener('DOMContentLoaded', function() {
     var el = document.querySelector('.title');
     el.addEventListener('click', function() {
         alert('Laravel!!!');
